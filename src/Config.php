435ceefb76a1d193e9fee1aec53a961749d98bf4
ghostff <frankchris@hotmail.com>
2017-02-16 12:09:36 -0600

    Update Config file

--- src/Config.php
+++ src/Config.php
@@ -1,19 +1,20 @@
 <?php
+
 return [
     'driver' => 'file',
 
     'file'  => [
         'salt'          => 'secret_salt_key',   #
         'path'          => '/',                 #
-		'rotate'		=> 1800,				# Rotate every 30 min(60 * 30). 
+	'rotate'	=> 1800,		# Rotate every 30 min(60 * 30). 
         'domain'        => null,                #
         'http_only'     => true,                #
         'expiration'    => 0,                   #
 		
-		'name'			=> '_Bittr_SESSID',		#
+	'name'		=> '_Bittr_SESSID',	#
         'match_ip'      => true,                #
         'match_browser' => true,                #
-		'save_path'     => 'Temp/',             #
+	'save_path'     => 'Temp/',     	#
         'cache_limiter' => 'nocache'           	#
 		
     ],
